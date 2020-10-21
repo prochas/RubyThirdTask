303d77db13cec9c7da74604d53ebefdbb0bb6b6d
prochas <50612196+prochas@users.noreply.github.com>
2020-10-21 16:36:01 +0300

    Add files via upload

---
+++ testas_main2.rb
@@ -0,0 +1,13 @@
+require_relative 'testas2.rb'
+
+# Aprasomi kintamieji
+a = 0
+s = 0
+atb1 = 0
+atb2 = 0
+kvad = 0
+sk = 1
+kiekis = 1
+
+value = TestasMetodas2.new
+value.skaiciavimas(kiekis, a, s, atb1, atb2, kvad, sk)
